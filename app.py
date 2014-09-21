7f67e8fb3631a327981a3b488c4c3f83129a13fa
Arnoldnuo <arnoldnuo@gmail.com>
2014-09-21 19:08:15 +0800

    添加文档

--- app.py
+++ app.py
@@ -91,7 +91,6 @@ def pageHtml(id):
     article['title'] = article['title'].replace('.txt','')
     return render_template('page.html',article = article, images = images, news=articleList[0:10], hots = articleList[10:20]) 
 
-'''
 if __name__ == '__main__':
-    app.run()
-'''
+    app.debug = True
+    app.run(host="0.0.0.0")
